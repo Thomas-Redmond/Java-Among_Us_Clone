61f03eff9ec5e4bf325d9d0979efcea31c30a932
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-08 17:25:03 +0000

    example file to help, and some masterpage

---
+++ example.java
@@ -0,0 +1,11 @@
+// empty java file
+public example{
+  // replace "example" with the name of the file, excludingthe extension
+
+  public static void main(String[] args){
+    // main function that will be used if file is directly called by command line
+    // takes a list of strings stored in variable arguments which are the command line parameters passed to function
+
+    // do something here will likley be used for testing lots
+  }
+}
---
+++ masterpage.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class masterpage{
+  // declare variables outside of main function - causes less errors. Assign values in constructor
+
+
+  // Constructor
+  public void constructor(){
+    Frame newFrame = new Frame("Master Page");
+  }
+
+    public static void main(String[] args){
+      // if called as the main function do this
+      // use for testing purposes
+    }
+}
+// java application master page
