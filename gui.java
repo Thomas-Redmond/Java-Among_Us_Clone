cd7ec12fabe6cf1ca3c07f30d569c63c0f39e64c
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-09 10:36:53 +0000

    trying to work out best formatting

---
+++ gui.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class gui {
+
+
+  public static void main (String[] args) {
+    // testing purposes
+    gui game = new gui();
+
+  }
+
+  public class gui_masterpage{
+
+  }
+}
