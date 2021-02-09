8f0d70d7db5da617c7b2626bdc2ffcc541e0a675
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-09 10:39:26 +0000

    Made run document

---
+++ run.java
@@ -0,0 +1,11 @@
+// begin the application
+// access various modules like GUI throguh packages
+
+public class run{
+
+  public static void main(String[] args){
+    // do this to start game
+    
+  }
+
+}
