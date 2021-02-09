c82c2e2b1c176d2ec6ab395a75ebcbd807758b8c
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-09 10:50:09 +0000

    more messing around with layout

---
+++ client.java
@@ -0,0 +1,12 @@
+// Domain of Team B - Client server communication
+// probably best to have 1 server file
+// 1 client file for simplicity / performance
+
+// compilation code: javac -d . server.java
+
+package network;
+public class client{
+  public static void main(String[] args){
+    // do something here
+  }
+}
