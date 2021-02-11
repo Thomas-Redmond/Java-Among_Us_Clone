9a8bba84bda113e4d3cfd2e8036ecc2cfbdd0774
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-11 12:32:17 +0000

    hello there, new files set up

---
+++ network.java
@@ -0,0 +1,12 @@
+package compiledGame;
+
+// compilation help
+// javac -d . network.java
+// running help for testing on local machines
+// java compiledGame.network
+
+public class network{
+  public static void main(String[] args){
+    // do something
+  }
+}
