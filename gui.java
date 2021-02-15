e8dbeca06a8785c958739e859b5e402616a33dd0
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-02-15 12:29:13 +0000

    new implementation of window closing

--- gui.java
+++ gui.java
@@ -6,19 +6,17 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+/*
+This function is the framework class. Think of it as the bucket which contains GUI soup. If the bucket isn't there it would make a very big mess
+It is not supposed to be visible, and works in the background. It will not have widgets.
+*/
 public class gui extends Frame implements WindowListener{
+  // declare variables before implementation to avoid ERROR: not found
 
-  private void constructor(String newTitle){
-    Frame window = new Frame("New Window");
-
-    setTitle(newTitle);
-    setVisible(true);
-    setSize(700,700);
-    setLayout(null);
-    addWindowListener(this); // required to close the window
-    // declaring generic elements
-
-    // adding elements to frame
+  // constructor method
+  private void gui(){
+    page1 newPage = new page1();
+    newPage.setVisible(true);
   }
 
   // following methods are all required to close the window
@@ -26,17 +24,41 @@ public class gui extends Frame implements WindowListener{
   public void windowActivated(WindowEvent e) {}
   public void windowClosed(WindowEvent e) {}
   public void windowClosing(WindowEvent e) {
-      dispose();
+    dispose();
   }
   public void windowDeactivated(WindowEvent e) {}
   public void windowDeiconified(WindowEvent e) {}
   public void windowIconified(WindowEvent e) {}
   public void windowOpened(WindowEvent arg0) {}
-  // end of window methods
+  // end of MANDATORY window methods
 
   public static void main(String[] args){
     gui master = new gui();
-    master.constructor("Generic Page");
+    master.setVisible(true);
+    master.addWindowListener(master);
+    // instansiate child classes
+
   }
 
+private class page1 extends gui{
+  // declare variables here - values assigned elsewhere unless they're like pi
+  Label title;
+
+  // constructor !
+  private void page1(){
+    setTitle("FRAME");
+    addWindowListener(this);
+    // instansiate variables here
+    title = new Label();
+
+    // declare text for Labels
+    title.setText("New String");
+
+    // give position data
+    title.setAlignment(Label.CENTER);
+    // add to page
+
+  }
+
+  }
 }
