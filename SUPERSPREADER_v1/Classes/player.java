7a873da3b1e58dedc13fe4a6fc283359be88efea
Devaang Misra <devaang@Devaangs-MacBook-Pro.local>
2021-03-17 11:12:46 +0000

    added picture to player

---
+++ SUPERSPREADER_v1/Classes/player.java
@@ -0,0 +1,66 @@
+package Classes;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+
+public class player extends entity{
+
+	
+	int velx=0,vely=0;
+	public player(int x, int y) {
+		super(x, y);
+		
+	}
+	
+	public void update()
+	{
+		y+=vely;
+		x+=velx;
+		
+	}
+	
+	public void draw(Graphics2D g2d) {
+		g2d.drawImage(getPlayerImg(),x,y,null);
+	}
+	
+	public Image getPlayerImg() {
+		ImageIcon ic = new ImageIcon("Objects/blobby.jpg");
+		Image i = ic.getImage();
+		Image si= i.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+		ic= new ImageIcon(si);
+		return si;
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		int key=e.getKeyCode();
+		
+		if (key == KeyEvent.VK_W) {
+			vely = -2;
+		}else if (key == KeyEvent.VK_S) {
+			vely = 2;
+		}else if(key == KeyEvent.VK_A) {
+			velx = -2;
+		}else if(key == KeyEvent.VK_D) {
+			velx= 2;
+		}
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		int key=e.getKeyCode();
+		
+		if (key == KeyEvent.VK_W) {
+			vely = 0;
+		}else if (key == KeyEvent.VK_S) {
+			vely = 0;
+		}else if(key == KeyEvent.VK_A) {
+			velx = 0;
+		}else if(key == KeyEvent.VK_D) {
+			velx= 0;
+		}
+	}
+
+}
+
